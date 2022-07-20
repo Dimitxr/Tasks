4defbe1e06ee0650a03ef54536367ac8a51dad15
Dimitxr <92766449+Dimitxr@users.noreply.github.com>
2022-07-20 22:09:51 +0300

    Add files via upload

---
+++ Task1andTask2/StartUp/MovieStar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    public class MovieStar
+    {
+        private string name;
+        private string surname;
+        private DateTime dateOfBirth;
+        private string sex;
+        private string nationality;
+        public MovieStar(string name, string surname, DateTime dateOfBirth, string sex, string nationality)
+        {
+            Name = name;
+            Surname = surname;
+            DateOfBirth = dateOfBirth;
+            Sex = sex;
+            Nationality = nationality;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set { dateOfBirth = value; }
+        }
+        public string Sex
+        {
+            get { return sex; }
+            set { sex = value; }
+        }
+        public string Nationality
+        {
+            get { return nationality; }
+            set { nationality = value; }
+        }
+    }
+}
---
+++ Task1andTask2/Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grossValue = int.Parse(Console.ReadLine());
+            double netValue;
+            double tax = 0;
+            double socialContribution = 0;
+
+            if (grossValue <= 1000)
+            {
+                netValue = grossValue;
+            }
+            // CALCULATING THE TAX 
+            // If it's needed in the future, we can add more taxes with if-esle statements,depending on their specifications.
+            // Another way is to create a method for the taxes like the SocialContributionRounding.
+            else if (grossValue > 1000)
+            {
+                tax = (grossValue - 1000) * 0.1;
+            }
+
+            if (grossValue - 1000 < 3000)
+            {
+                double roundedValue = SocialContributionRounding(grossValue);
+                socialContribution = roundedValue * 0.15;
+            }
+
+            netValue = grossValue - tax - socialContribution;
+
+            Console.WriteLine($"Net Salary = {netValue}");
+            Console.WriteLine($"Tax = {tax}");
+            Console.WriteLine($"Social Contribution = {socialContribution}");
+        }
+
+        public static double SocialContributionRounding(double grossValue)
+        {
+            double value = 0;
+            if (grossValue - 1000 > 2000 && grossValue - 1000 < 3000)
+            {
+                value = 2000;
+            }
+            else if (grossValue - 1000 > 1000 && grossValue - 1000 < 2000)
+            {
+                value = 1000;
+            }
+            return value;
+        }
+    }
+}
